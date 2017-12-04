85dc8f0c8db318f31c949e2733c2ed12a730ad24
jkolios <jkolios@gophigital.com>
2017-12-04 14:33:27 +0200

    added TERM info to the DB scrript

--- ci/create_database.sh
+++ ci/create_database.sh
@@ -1,2 +1,3 @@
+export TERM=xterm-256color
 docker build -t 552796280524.dkr.ecr.eu-central-1.amazonaws.com/populated.postgis:empty .
 docker push 552796280524.dkr.ecr.eu-central-1.amazonaws.com/populated.postgis:empty
